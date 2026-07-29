0956012a9d0a1d91fc6aef72748b7142adaae8e7
Juloc <juli.hiresch@gmail.com>
2026-07-30 00:29:16 +0200

    Install Julgate path guard through hosting startup

---
+++ Matgate/Web/PathSecurityHostingStartup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Matgate.Web.PathSecurityHostingStartup))]
+
+namespace Matgate.Web;
+
+public sealed class PathSecurityHostingStartup : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddTransient<IStartupFilter, PathSecurityStartupFilter>();
+        });
+    }
+
+    private sealed class PathSecurityStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UseMiddleware<PathTraversalGuardMiddleware>();
+                next(app);
+            };
+        }
+    }
+}
