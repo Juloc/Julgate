cb2c29a1aba1f5604bf15b5323f531ac06767004
Juloc <juli.hiresch@gmail.com>
2026-07-30 00:51:53 +0200

    Test non-routable Julgate proxy targets

--- Matgate.Tests/WebsiteProxyTargetGuardTests.cs
+++ Matgate.Tests/WebsiteProxyTargetGuardTests.cs
@@ -7,12 +7,15 @@ namespace Matgate.Tests;
 public sealed class WebsiteProxyTargetGuardTests
 {
     [Theory]
+    [InlineData("0.0.0.0")]
     [InlineData("127.0.0.1")]
     [InlineData("169.254.169.254")]
     [InlineData("100.100.100.200")]
     [InlineData("168.63.129.16")]
+    [InlineData("224.0.0.1")]
+    [InlineData("fe80::1")]
     [InlineData("fd00:ec2::254")]
-    public void MetadataAndLoopbackAddresses_AreBlocked(string value)
+    public void NonRoutableAndMetadataAddresses_AreBlocked(string value)
     {
         Assert.True(WebsiteProxyTargetGuardMiddleware.IsDisallowedAddress(IPAddress.Parse(value)));
     }
